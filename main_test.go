49db13e7401b5345fac6b74217bc5176e88d375c
Sébastien Job <sebastienjob@gmail.com>
2022-10-18 17:22:25 +0200

    Fix unit tests

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"io"
+	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -11,49 +13,117 @@ import (
 )
 
 func TestGatherItems(t *testing.T) {
-	ctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	panicOnError(err)
-
-	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-	panicOnError(err)
-	defer reader.Close()
-	io.Copy(io.Discard, reader)
-
-	created, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "docker.io/library/alpine",
-		Tty:   false,
-	}, nil, nil, nil, "")
-	panicOnError(err)
-
-	err = cli.ContainerStart(ctx, created.ID, types.ContainerStartOptions{})
-	panicOnError(err)
-
-	cli.ContainerWait(ctx, created.ID, container.WaitConditionNotRunning)
-	// panicOnError(err)
-
-	items, err := gatherItems(cli)
-	panicOnError(err)
-
-	found := false
-	for _, item := range items.Containers {
-		if item.ID == created.ID {
-			found = true
-		}
+	tc, cancel := BuildTestingContext(t)
+	defer cancel()
+	tc.PullImage("docker.io/library/alpine")
+	id := tc.CreateContainer("docker.io/library/alpine")
+	tc.StartContainer(id)
+	tc.StopContainer(id)
+	tc.WaitContainerStopped(id)
+	items, err := gatherItems(tc.cli)
+	if err != nil {
+		t.Fatal(err)
 	}
-	err = cli.ContainerStop(ctx, created.ID, nil)
-	panicOnError(err)
-
-	err = cli.ContainerRemove(ctx, created.ID, types.ContainerRemoveOptions{RemoveVolumes: true, Force: true})
-	panicOnError(err)
-
-	if !found {
+	found := ItemsContainId(items.Containers, id)
+	tc.RemoveContainer(id)
+	if found {
+		t.Log("Created container found")
+	} else {
 		t.Fatal("Created container not found")
 	}
 }
 
-func panicOnError(err error) {
-	if err != nil {
-		panic(err)
+func ItemsContainId(items []item, ID string) bool {
+	for _, item := range items {
+		if item.ID == ID {
+			return true
+		}
 	}
+	return false
+}
+
+func BuildTestingContext(t *testing.T) (TestingContext, context.CancelFunc) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatal(err)
+	}
+	return TestingContext{t: t, ctx: ctx, cli: cli}, cancel
+}
+
+type TestingContext struct {
+	t   *testing.T
+	ctx context.Context
+	cli *client.Client
+}
+
+func (tc *TestingContext) PullImage(image string) {
+	reader, err := tc.cli.ImagePull(tc.ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		tc.t.Fatal(err)
+	}
+	defer reader.Close()
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		var jsonLine map[string]any
+		err = json.Unmarshal([]byte(line), &jsonLine)
+		if err != nil {
+			tc.t.Log(line)
+		} else {
+			if jsonLine["progress"] != nil {
+				tc.t.Log(jsonLine["status"], jsonLine["progress"])
+			} else {
+				tc.t.Log(jsonLine["status"])
+			}
+		}
+	}
+}
+
+func (tc *TestingContext) CreateContainer(image string) string {
+	created, err := tc.cli.ContainerCreate(tc.ctx, &container.Config{
+		Image: image,
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		tc.t.Fatal(err)
+	}
+	tc.t.Log("Container", created.ID, "created")
+	return created.ID
+}
+
+func (tc *TestingContext) StartContainer(ID string) {
+	err := tc.cli.ContainerStart(tc.ctx, ID, types.ContainerStartOptions{})
+	if err != nil {
+		tc.t.Fatal(err)
+	}
+	tc.t.Log("Container", ID, "started")
+}
+
+func (tc *TestingContext) StopContainer(ID string) {
+	err := tc.cli.ContainerStop(tc.ctx, ID, nil)
+	if err != nil {
+		tc.t.Fatal(err)
+	}
+	tc.t.Log("Container", ID, "stop requested")
+}
+
+func (tc *TestingContext) WaitContainerStopped(ID string) {
+	statusCh, errCh := tc.cli.ContainerWait(tc.ctx, ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			tc.t.Fatal(err)
+		}
+	case <-statusCh:
+	}
+	tc.t.Log("Container", ID, "stopped")
+}
+
+func (tc *TestingContext) RemoveContainer(ID string) {
+	err := tc.cli.ContainerRemove(tc.ctx, ID, types.ContainerRemoveOptions{RemoveVolumes: true, Force: true})
+	if err != nil {
+		tc.t.Fatal(err)
+	}
+	tc.t.Log("Container removed")
 }
